5ceb62b52af58306c0bc5cee05d88d2e0a9902fc
yetzt <git@yetzt.me>
2016-04-14 16:41:46 +0200

    amab server

---
+++ server.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+// node modules
+var path = require("path");
+var fs = require("fs");
+
+// npm modules
+var ellipse = require("ellipse");
+
+// clean up old socket
+if (fs.existsSync(path.resolve(__dirname, "server.sock"))) fs.unlinkSync(path.resolve(__dirname, "server.sock"));
+
+// load data
+var data = JSON.parse(fs.readFileSync(path.resolve(__dirname, "assets/data/data.json")));
+
+// instance of app
+var app = ellipse();
+
+// tile route
+app.get("/random/:z(\\d+)/:x(\\d+):y(\\d+)", function(req, res){
+	res.redirect(data[Math.floor(Math.random()*data.length)].replace("{x}", req.params.x).replace("{y}", req.params.y).replace("{z}", req.params.z));
+});
+
+// default route
+app.get("*", function(req,res){
+	res.status(404).end("nope");
+});
+
+// listen
+module.exports = app.listen(path.resolve(__dirname, "server.sock"), function() {
+	fs.chmod(path.resolve(__dirname, "server.sock"), 0777);
+});
